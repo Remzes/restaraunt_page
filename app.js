95202abb0f9180cab6c9052e5c61faf617a3755f
Remzes <victor_s1998@mail.ru>
2017-10-02 06:22:41 +0300

    First Commit

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+const config = require('./config');
+const bodyParser = require('body-parser');
+
+const options = {
+    user: config.MYSQL_USER,
+    password: config.MYSQL_PASSWORD,
+    database: "RESTARAUNT_DATABASE"
+};
 
 const app = express();
 
-app.get((req, res) => {
-   console.log("Rendering is completed...");
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname + "/views"));
+
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection(options);
+
+app.get("/", (req, res) => {
+    const query_one = "SELECT * FROM provinces";
+    const query_two = "SELECT * FROM food_combos";
+    let items = "";
+    let combos = "";
+
+    connection.query(query_one, async (err, result) => {
+        if (err) throw  err;
+        items = result;
+    });
+
+    connection.query(query_two, async (err, result) => {
+        if (err) throw err;
+        combos = result
+    });
+
+    setTimeout(() => {
+        res.render("index", {items: items, combos: combos})
+    }, 500);
+
 });
 
-app.use(express.static(__dirname + "/public"));
 
 // Start the server
 const PORT = process.env.PORT || 8080;
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    GLOUD_PROJECT: "restarauntpage",
+    DATA_BACKEND: "cloudsql",
+    MYSQL_USER: "viktor",
+    MYSQL_PASSWORD: "Vvictor1998",
+    INSTANCE_CONNECTION_NAME: "restarauntpage:us-east4:restarauntpagedb"
+};
